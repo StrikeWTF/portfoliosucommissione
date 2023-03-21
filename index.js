9f9c40d699e9ad92aa3b47d9f3cb960da819180c
Massimo Conte <strikeonod@gmail.com>
2023-03-21 17:53:08 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function GHRedirect(){
+    //window.location.href = "https://github.com";
+    window.location.replace("https://github.com")
+}
+
+function AboutMeRedirect() {
+    window.location.href = "https://it.wikipedia.org/wiki/Mostro_di_Firenze";
+}
